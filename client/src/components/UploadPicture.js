186cb4244923c7b3417797bab1b6bc9112485813
Sarkar, Pompita (US - Bengaluru) <pomsarkar@deloitte.com>
2019-07-20 12:18:22 +0530

    Added upload picture

--- client/src/components/UploadPicture.js
+++ client/src/components/UploadPicture.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import $ from 'jquery';
 
 export default class UploadPicture extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedFile: null,
+      lat: null,
+      lng: null,
+      b64: null,
     };
   }
 
@@ -18,11 +22,35 @@ export default class UploadPicture extends React.Component {
       selectedFile: event.target.files[0],
       loaded: 0,
     });
+    if (event.target.files && event.target.files[0]) {
+      const FR = new FileReader();
+      FR.addEventListener('load', (e) => {
+        this.state.b64 = e.target.result;
+        // img = e.target.result;
+      });
+      FR.readAsDataURL(event.target.files[0]);
+    }
   }
 
   onClickHandler = () => {
     const data = new FormData();
     data.append('file', this.state.selectedFile);
+    this.validateForm();
+  }
+
+  validateForm = () => {
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:7000/api/users',
+      success(result) {
+        console.log('Success', result);
+      },
+      data: {
+        lat: this.state.lat,
+        lng: this.state.lng,
+        b64: this.state.b64,
+      },
+    });
   }
 
   render() {
